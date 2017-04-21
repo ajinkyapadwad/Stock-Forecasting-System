2788535d0de204edd606174a1954652a12027d75
Ajinkya Padwad <ajinkyapadwad@gmail.com>
2017-04-21 19:39:50 -0400

    Update urls.py

--- realtime/urls.py
+++ realtime/urls.py
@@ -8,8 +8,9 @@ from . import views
 urlpatterns = [
 
     url(r'^index', views.index, name='index'),
-    url(r'^prediction', views.prediction, name='prediction'),
+    url(r'^pred', views.pred, name='pred'),
     url(r'^news', views.news, name='news'),
     url(r'^contact', views.contact, name='contact'),
-    url(r'^newprediction', views.newprediction, name='newprediction'),
+    url(r'^passing', views.passing, name='passing'),
+    #url( r'^search/(?P<name>[a-zA-Z0-9_.-]+)/$', views.search, name='search_stock' ),
 ]
